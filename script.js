0cb730cbc5845ce00ea6894b199bb98f0275aab8
Carla Íncola <77678731+CarlaIncola@users.noreply.github.com>
2022-08-11 18:34:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,154 @@
+var quotes =[
+{"quote":"The chief enemy of creativity is ‘good’ sense.", "name":"Pablo Picasso"},
+{"quote":"Bring your humanity to your art. Bring your art to humanity.", "name":"Maxime Lagacé"},
+{"quote":"Art deteriorates when it is done for the audience.", "name":"James Pierce"},
+{"quote":"In order to be irreplaceable one must always be different.", "name":"Coco Chanel"},
+{"quote":"I had so much fire in me and so many plans.", "name":"Claude Monet"},
+{"quote":"The only thing I know is that I paint because I need to.", "name":"Frida Kahlo"},
+{"quote":"Art enables us to find ourselves and lose ourselves at the same time.", "name":"Thomas Merton"},
+{"quote":"To draw you must close your eyes and sing.", "name":"Pablo Picasso"},
+{"quote":"Never worry about being obsessive. I like obsessive people. Obsessive people make great art.", "name":"Susan Sontag"},
+{"quote":"The world always seems brighter when you’ve just made something that wasn’t there before.", "name":"Neil Gaiman"},
+{"quote":"Every act of creation is first an act of destruction.", "name":"Pablo Picasso"},
+{"quote":"Art is the only serious thing in the world. And the artist is the only person who is never serious.", "name":"Oscar Wilde"},
+{"quote":"If I paint something, I don’t want to have to explain what it is.", "name":"Bob Ross"},
+{"quote":"I never paint dreams or nightmares. I paint my own reality.", "name":"Frida Kahlo"},
+{"quote":"Creativity is allowing yourself to make mistakes. Art is knowing which ones to keep.", "name":"Scott Adams"},
+{"quote":"Where the spirit does not work with the hand, there is no art.", "name":"Leonardo Da Vinci"},
+{"quote":"Art is standing with one hand extended into the universe and one hand extended into the world, and letting ourselves be a conduit for passing energy.", "name":"Albert Einstein"},
+{"quote":"You have to systematically create confusion, it sets creativity free. Everything that is contradictory creates life.", "name":"Salvador Dalí"},
+{"quote":"The best reason to paint is that there is no reason to paint.", "name":"Keith Haring"},
+{"quote":"If you ask me what I came to do in this world, I, an artist, will answer you: I am here to live out loud.", "name":"Emile Zola"},
+{"quote":"Art is not a mirror to hold up to society, but a hammer with which to shape it.", "name":"Leon Trotsky"},
+{"quote":"The principles of true art is not to portray, but to evoke.", "name":"Jerzy Kosinski"},
+{"quote":"I wanted to start a revolution, using art to build the sort of society I myself envisioned.", "name":"Yayoi Kusama"},
+{"quote":"Every child is an artist.  The problem is how to remain an artist once he grows up.", "name":"Pablo Picasso"},
+{"quote":"A work is finished when an artist realizes his intentions.", "name":"Rembrandt"},
+{"quote":"Art wasn’t supposed to look nice; it was supposed to make you feel something.", "name":"Rainbow Rowell"},
+{"quote":"Art wasn’t supposed to look nice; it was supposed to make you feel something.", "name":"Joan Miro"},
+{"quote":"Art speaks where words are unable to explain.", "name":"Unknown"},
+{"quote":"Art is never finished, only abandoned.", "name":"Leonardo Da Vinci"},
+{"quote":"Art is the concrete representation of our most subtle feelings.", "name":"Agnes Martin"},
+{"quote":"Everything you can imagine is real.", "name":"Pablo Picasso"},
+{"quote":"The only art I’ll ever study is stuff that I can steal from.", "name":"David Bowie"},
+{"quote":"I put my heart and my soul into my work, and have lost my mind in the process.", "name":"Vincent Van Gogh"},
+{"quote":"If you hear a voice within you say “you cannot paint,” then by all means paint and that voice will be silenced.", "name":"Vincent Van Gogh"},
+{"quote":"Art is my life and my life is art.", "name":"Yoko Ono"},
+{"quote":"You don’t take a photograph, you make it.", "name":"Ansel Adams"},
+{"quote":"Have no fear of perfection – you’ll never reach it.", "name":"Salvador Dalí"},
+{"quote":"Those who do not want to imitate anything, produce nothing.", "name":"Salvador Dalí"},
+{"quote":"If people knew how hard I worked to get my mastery, it wouldn’t seem so wonderful at all.", "name":"Michelangelo"},
+{"quote":"Beauty will save the world.", "name":"Dosteovsky"},
+{"quote":"Art must destroy violence, only it can do it.", "name":"Leo Tolstoy"},
+{"quote":"Creativity is contagious. Pass it on.", "name":"Albert Einstein"},
+{"quote":"Art is the expression of the profoundest thoughts in the simplest way.", "name":"Albert Einstein"},
+{"quote":"Art is to look at not to criticize.", "name":"Edgar Allen Poe"},
+{"quote":"Sometimes with art, it is important just to look.", "name":"Marina Abramović"},
+{"quote":"Controversy is part of the nature of art and creativity.", "name":"Yoko Ono"},
+{"quote":"Art is a lie that makes us realize truth.", "name":"Pablo Picasso"},
+{"quote":"The painter has the Universe in his mind and hands.", "name":"Leonardo Da Vinci"},
+{"quote":"The painter has the Universe in his mind and hands.", "name":"Leonardo Da Vinci"},
+{"quote":"Think eight hours, work two hours.", "name":"Mirko Ilic"},
+{"quote":"To create one’s own world takes courage.", "name":"Georgia O’Keeffe"},
+{"quote":"It is absurd to look for perfection.", "name":"Camille Pissarro"},
+{"quote":"Life is too short to remain unnoticed.", "name":"Salvador Dalí"},
+{"quote":"Art must be life, it must belong to everybody.", "name":"Marina Abramović"},
+{"quote":"To avoid criticism, do nothing, say nothing, and be nothing.", "name":"Elbert Hubbard"},
+{"quote":"To create, one must first question everything.", "name":"Eileen Gray"},
+{"quote":"Everything starts from a dot.", "name":"Wassily Kandinsky"},
+{"quote":"To gain freedom is to gain simplicity.", "name":"Joan Miro"},
+{"quote":"The more I work, the more I want to work.", "name":"Joan Miro"},
+{"quote":"At the heart of all great art is an essential melancholy.", "name":"Federico Garcia Lorca"},
+{"quote":"Inspiration does exist, but it must find you working.", "name":"Pablo Picasso"},
+{"quote":"Don’t be fooled by success and money. Don’t let anything come between you and your work.", "name":"Louise Bourgeois"},
+{"quote":"When you do things for their own sake, that’s when you create your best work. That’s when it’s art.", "name":"Naval Ravikant"},
+{"quote":"Art is not a handicraft, it is the transmission of feeling the artist has experienced.", "name":"Leo Tolstoy"},
+{"quote":"To be great, art has to point somewhere.", "name":"Anne Lamott"},
+{"quote":"When I think of art I think of beauty. Beauty is the mystery of life. It is not in the eye it is in the mind. In our minds there is awareness of perfection.", "name":"Agnes Martin"},
+{"quote":"All you need to paint is a few tools, a little instruction, and a vision in your mind.", "name":"Bob Ross"},
+{"quote":"A true artist is not one who is inspired but one who inspires others.", "name":"Salvador Dalí"},
+{"quote":"Make art that makes you happy. Make art that makes you better. Make art that makes you money. Make art that makes a difference.", "name":"Sahil Lavingia"},
+{"quote":"If I’m going to be anything more than average, if anyone is going to remember me, then I need to go further, in art, in life, in everything!", "name":"Salvador Dalí"},
+{"quote":"The only way to be truly satisfied is to do what you believe is great work.", "name":"Steve Jobs"},
+{"quote":"It is not a torment to be an artist. It is a privilege.", "name":"Louise Bourgeois"},
+{"quote":"Art is the queen of all sciences communicating knowledge to all the generations of the world", "name":"Leonardo Da Vinci"},
+{"quote":"With the pride of the artist, you must blow against the walls of every power that exists the small trumpet of your defiance.", "name":"Norman Mailer"},
+{"quote":"The artist must create a spark before he can make a fire and before art is born, the artist must be ready to be consumed by the fire of his own creation.", "name":"Auguste Rodin"},
+{"quote":"He who works with his hands and his head and his heart is an artist.", "name":"Francis of Assisi"},
+{"quote":"Instead of worrying about what people say of you, why not spend time trying to accomplish something they will admire.", "name":"Dale Carnegie"},
+{"quote":"It’s on the strength of observation and reflection that one finds a way. So we must dig and delve unceasingly.", "name":"Claude Monet"},
+{"quote":"To practice any art, no matter how well or badly, is a way to make your soul grow. So do it.", "name":"Kurt Vonnegut"},
+{"quote":"Stay firmly in your path and dare; be wild two hours a day.", "name":"Paul Gauguin"},
+{"quote":"Visions are worth fighting for. Why spend your life making someone else’s dreams?", "name":"Tim Burton"},
+{"quote":"Not everyone can become a great artist, but a great artist can come from anywhere.", "name":"Peter O’Toole"},
+{"quote":"Great things are not done by impulse, but by a series of small things brought together.", "name":"Vincent Van Gogh"},
+{"quote":"By working hard, old man, I hope to make something good one day. I haven’t yet, but I am pursuing it and fighting for it.", "name":"Vincent Van Gogh"},
+{"quote":"One must spoil as many canvases as one succeeds with.", "name":"Vincent Van Gogh"},
+{"quote":"We don’t make mistakes. We just have happy accidents.", "name":"Bob Ross"},
+{"quote":"We have always been shameless about stealing great ideas.", "name":"Steve Jobs"},
+{"quote":"It’s better to create something that others criticize than to create nothing and criticize others.", "name":"Ricky Gervais"},
+{"quote":"I just wanna make the world a better-looking place. If you don’t like it, you can paint over it!", "name":"Bansky"},
+{"quote":"People say graffiti is ugly, irresponsible and childish… but that’s only if it’s done properly.", "name":"Bansky"},
+{"quote":"There are three responses to a piece of design: yes, no and wow.", "name":"Milton Glaser"},
+{"quote":"The fact I myself do not understand what my paintings mean while I am painting them does not imply that they are meaningless.", "name":"Salvador Dalí"},
+{"quote":"What is originality? Undetected plagiarism.", "name":"William Ralph Inge"},
+{"quote":"Artists are just children who refuse to put down their crayons.", "name":"Al Hirschfeld"},
+{"quote":"The critic has to educate the public; the artist has to educate the critic.", "name":"Oscar Wilde"},
+{"quote":"Bus stops are far more interesting and useful places to have art than in museums.", "name":"Bansky"},
+{"quote":"You must have chaos within you to give birth to a dancing star.", "name":"Friedrich Nietzsche"},
+{"quote":"My paintings are not about what is seen. They are about what is known forever in the mind.", "name":"Agnes Martin"},
+{"quote":"If art doesn’t make us better, then what on earth is it for?", "name":"Alice Walker"},
+{"quote":"Artists are people driven by the tension between the desire to communicate and the desire to hide.", "name":"Donald Winnicott"},
+{"quote":"Forget yourself. Become one with eternity. Become part of your environment.", "name":"Yayoi Kusama"},
+{"quote":"The creative union of the conscious with the unconscious is what one usually calls ‘inspiration’.", "name":"Leon Trotsky"},
+{"quote":"What is important is to spread confusion, not eliminate it.", "name":"Salvador Dalí"},
+{"quote":"Not everyone is capable of madness; and of those lucky enough to be capable, not many have the courage for it.", "name":"August Strindberg"},
+{"quote":"It is better to have done something than to have been someone.", "name":"Claude Monet"}
+];
+
+//color list
+var colors = ["B6E2DD","C8DDBB","#E9E5AF","#FBDF9D","#FBC99D", "FBB39D", "#FBA09D", "#D8E2DC", "#FFE5D9", "#FFCAD4", "#F4ACB7", "#CCD5AE", "#E9EDC9", "#FEFAE0", "#FAEDCD", "#E7C8A0", "#D4A373", "#E5CDB0", "#F3D6C2", "#FBF1EA", "#EFEDE6", "#D7D3C1", "#ffe8d6", "#f8f4e3"];
+
+//html full load
+$(document).ready(function(){
+  
+  // function to random quote
+  var randomQuote = function(id, colorRand){
+  // when fadeOut -> then fadeIn and animate
+  $("#quote, #name").fadeOut(1000, function(){
+    //quote actions
+    $("#quote").css("color", colorRand).fadeIn(1000).html('<i class="fa fa-quote-left" aria-hidden="true"></i> '+quotes[id].quote+' <i class="fa fa-quote-right" aria-hidden="true"></i>');
+     
+    //name actions
+    $("#name").css("color", colorRand).fadeIn(1000).html("- "+quotes[id].name+"");
+    
+    //twitter share 
+    $("#twitter").css("color", colorRand).fadeIn(1000).html('<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text='+quotes[id].quote+" - "+quotes[id].name+'" data-size="large" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>'); 
+     
+    //change link color in every color random
+    $("a").animate({
+      color: colorRand
+    });
+    
+    // animate from jQuery UI
+    $("body, button").animate({
+      backgroundColor: colorRand
+    });
+    
+   });
+ };
+  
+  //init
+  var colorRand =  colors[Math.floor(Math.random() * colors.length)];
+  var id = Math.floor(Math.random() * quotes.length);
+  randomQuote(id, colorRand);
+  
+  // when click
+$("#new-quote").click(function(){
+  colorRand =  colors[Math.floor(Math.random() * colors.length)];
+  console.log("out:"+ colorRand);
+  id = Math.floor(Math.random() * quotes.length);
+  randomQuote(id, colorRand);
+  });
+  
+});
